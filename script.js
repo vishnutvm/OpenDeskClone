2ff1c5824c7a9e60c6b3da9f3ef2ac8b5be8175e
vishnutvm <vishnuvichu8003@gmail.com>
2022-07-26 05:53:00 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+let toggleBtn= ()=>{
+    var elemnt = document.querySelector(".mobile-responsive");
+    var humMenu = document.querySelector(".humbger_menu ")
+  if(elemnt.style.display == "block"){
+    elemnt.style.display = "none"
+  }else{
+    elemnt.style.display = "block"
+    humMenu.style.color="gray"
+    
+    
+}
+
+ 
+}
+
